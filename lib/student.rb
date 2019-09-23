e5511007c4d0b7453694abf77ddfdb55bd96b554
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-23 16:09:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,16 +1,16 @@
 class Student < User
 
 
-  def new
-    @knowlegde = []
+  def initialize
+    @knowledge = []
   end 
   
   def learn(string)
-    @knowlegde << string
+    @knowledge << string
   end 
   
   def knowlegde
-    @knowlegde
+    @knowledge
   end 
   
   
